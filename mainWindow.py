a8887e7b72f773de98b71faf0685e9b85bc0e5db
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2011-02-01 00:34:25 +0100

    Add some accelerators.
    This should be rewrite. Directly write command from the mainWindow is a bad thing.

--- mainWindow.py
+++ mainWindow.py
@@ -56,11 +56,39 @@ class MainWindow(object):
 		vbox.child_set_property(self.entry, "expand", False)
 		self.workspaceContainer = gtk.VBox()
 		vbox.add(self.workspaceContainer)
-		
+
+		self.accelGroup = gtk.AccelGroup()
+		self.window.add_accel_group(self.accelGroup)
+	
+		self.entry.add_accelerator('grab-focus', self.accelGroup, *gtk.accelerator_parse("<Control>Return") , accel_flags = 0)
+		self.accelGroup.connect_group(*gtk.accelerator_parse("<Control>s"), accel_flags=0, callback = self.on_accel)
+		self.accelGroup.connect_group(*gtk.accelerator_parse("<Control>o"), accel_flags=0, callback = self.on_accel)
+		self.accelGroup.connect_group(*gtk.accelerator_parse("<Control>n"), accel_flags=0, callback = self.on_accel)
+		self.accelGroup.connect_group(*gtk.accelerator_parse("<Control>q"), accel_flags=0, callback = self.on_accel)
+
 		self.window.show_all()
 		self.helper = MainWindow.Helper(self.window)
 		
 		pass
+
+	def on_accel(self,accel_group, acceleratable, keyval, modifier):
+		if (keyval, modifier) == gtk.accelerator_parse("<Control>s"):
+			self.entry.set_text("save")
+			self.entry.activate()
+			return True
+		if (keyval, modifier) == gtk.accelerator_parse("<Control>o"):
+			self.entry.set_text("open")
+			self.entry.activate()
+			return True
+		if (keyval, modifier) == gtk.accelerator_parse("<Control>n"):
+			self.entry.set_text("new")
+			self.entry.activate()
+			return True
+		if (keyval, modifier) == gtk.accelerator_parse("<Control>q"):
+			self.entry.set_text("quit")
+			self.entry.activate()
+			return True
+		return False
 
 	def get_helper(self):
 		return self.helper
