91f4b34ad5af0ea47f1206d1b1894bfb0e4ead3d
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2011-02-28 11:21:17 +0100

    add GPLv3 license

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,3 +1,22 @@
+#    This file is part of CodeCollab.
+#
+#    Author: Matthieu Gautier <matthieu.gautier@mgautier.fr>
+#
+#    CodeCollab is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    CodeCollab is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with CodeCollab.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    Copyright 2011 Matthieu Gautier
 
 from actionDef import Action
 import controlerActions
