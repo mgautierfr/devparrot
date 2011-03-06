84c0a82d27af5307b7dbe528d20c73e63db0600d
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2011-03-06 15:38:15 +0100

    Switch project name from codecollab to devparrot.

--- devparrot.py
+++ devparrot.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 
-#    This file is part of CodeCollab.
+#    This file is part of DevParrot.
 #
 #    Author: Matthieu Gautier <matthieu.gautier@mgautier.fr>
 #
-#    CodeCollab is free software: you can redistribute it and/or modify
+#    DevParrot is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    CodeCollab is distributed in the hope that it will be useful,
+#    DevParrot is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with CodeCollab.  If not, see <http://www.gnu.org/licenses/>.
+#    along with DevParrot.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 #    Copyright 2011 Matthieu Gautier
@@ -30,7 +30,7 @@ from session import Session
 import mainWindow
 import controler
 
-class CodeCollab(object):
+class DevParrot(object):
 	def __init__(self):
 		mainWindow.init()
 		controler.init()
@@ -40,5 +40,5 @@ class CodeCollab(object):
 			command.run(sys.argv[1:])
 
 if __name__ == "__main__":
-	app = CodeCollab()
+	app = DevParrot()
 	gtk.main()
--- mainWindow.py
+++ mainWindow.py
@@ -1,19 +1,19 @@
-#    This file is part of CodeCollab.
+#    This file is part of DevParrot.
 #
 #    Author: Matthieu Gautier <matthieu.gautier@mgautier.fr>
 #
-#    CodeCollab is free software: you can redistribute it and/or modify
+#    DevParrot is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    CodeCollab is distributed in the hope that it will be useful,
+#    DevParrot is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with CodeCollab.  If not, see <http://www.gnu.org/licenses/>.
+#    along with DevParrot.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 #    Copyright 2011 Matthieu Gautier
@@ -93,7 +93,7 @@ def init():
 	icon_path = os.path.dirname(os.path.realpath(__file__))
 	icon_path = os.path.join(icon_path,"icon.png")
 	window.set_icon_from_file(icon_path)
-	window.set_title("CodeCollab")
+	window.set_title("DevParrot")
 	vbox = gtk.VBox()
 	window.add(vbox)
 	entry = gtk.Entry()
