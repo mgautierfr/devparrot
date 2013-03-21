c83cda069d2486c4de57d5f2272d0c10ff3c6b11
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2013-03-21 22:00:28 +0100

    add a shell command

---
+++ devparrot/commands/shell.py
@@ -0,0 +1,24 @@
+from devparrot.core.command.baseCommand import Command
+from devparrot.core.command import constraints, binder
+from devparrot.core import capi
+from devparrot.core.utils.posrange import Index
+
+@Command(
+stdinput = constraints.Stream()
+)
+def shell(command, stdinput, *args):
+    from subprocess import Popen, PIPE
+    commands = [command]+list(args)
+    print commands
+    popen = Popen([command]+list(args), bufsize=0, shell=False, stdin=PIPE, stdout=PIPE, universal_newlines=True)
+    outPipe = popen.stdout
+    inPipe = popen.stdin
+
+    for line in stdinput:
+        line = "%s\n"%line
+        inPipe.write(line)
+    inPipe.close()
+
+    for line in outPipe:
+        yield line
+
