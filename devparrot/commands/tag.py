5e86a5b1cbeb55eadd39f8d36b7d62a1bbfe7b5b
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2013-03-30 18:56:18 +0100

    add command tag and subcommand set

---
+++ devparrot/commands/tag.py
@@ -0,0 +1,16 @@
+
+from devparrot.core.command.masterCommand import MasterCommand, SubCommand
+from devparrot.core.command import constraints
+from devparrot.core import capi
+
+class tag(MasterCommand):
+
+    @SubCommand(
+    tagList = constraints.Stream()
+    )
+    def set(tagName, tagList):
+        tgList = [ str(item) for tuple_ in tagList for item in tuple_]
+        capi.currentDocument.model.tag_add(tagName, *tgList)
+
+
+
