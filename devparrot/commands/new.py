872d235d2f71b76bb893540cc14e3c8cb188aff7
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2013-04-01 12:01:12 +0200

    Command "new" can take a content stream and write it in the new buffer

--- devparrot/commands/new.py
+++ devparrot/commands/new.py
@@ -1,13 +1,19 @@
 from devparrot.core.command.baseCommand import Command
-from devparrot.core.command import binder
+from devparrot.core.command import constraints, binder
 from devparrot.core import capi
 
-@Command()
-def new():
+@Command(
+content = constraints.Stream()
+)
+def new(content):
     from devparrot.core.document import Document
     from devparrot.documents.newDocSource import NewDocSource
     document = Document(NewDocSource())
     capi.add_file(document)
     capi.currentDocument = document
+    model = document.get_model()
+
+    for line in content:
+        model.insert("insert", line)
 
 binder["<Control-n>"] = "new\n"
