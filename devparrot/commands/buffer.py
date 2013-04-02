1e9c3604b0a2bc85364522e3907d453e7e47ad51
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2013-04-02 23:19:28 +0200

    Add a buffer document source and a buffer command

---
+++ devparrot/commands/buffer.py
@@ -0,0 +1,20 @@
+from devparrot.core.command.baseCommand import Command
+from devparrot.core.command import constraints, binder
+from devparrot.core.commandLauncher import create_section
+from devparrot.core import capi
+
+def buffer(name, content):
+    from devparrot.core.document import Document
+    from devparrot.documents.bufferSource import BufferSource
+    document = Document(BufferSource(name))
+    capi.add_file(document)
+    capi.currentDocument = document
+    model = document.get_model()
+
+    for line in content:
+        model.insert("insert", line)
+
+
+Command(
+content = constraints.Stream()
+)(buffer, create_section("capi"))
---
+++ devparrot/documents/bufferSource.py
@@ -0,0 +1,59 @@
+#    This file is part of DevParrot.
+#
+#    Author: Matthieu Gautier <matthieu.gautier@mgautier.fr>
+#
+#    DevParrot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    DevParrot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with DevParrot.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    Copyright 2011 Matthieu Gautier
+
+class BufferSource(object):
+    """ This class is used for buffer document """
+    def __init__(self, name):
+        self.name = name
+        
+    def __getattr__(self, name):
+        if name in ["title", "longTitle"]:
+            return self.name
+        raise AttributeError
+
+    def __eq__(self, other):
+        if self.__class__ == other.__class__:
+            return self.name == other.name
+        return False
+        
+    def get_path(self):
+        return self.title
+
+    def has_path(self):
+        """
+        return True if the source has a path.
+        Always False for newDocSource
+        """
+        return False
+        
+    def get_content(self):
+        """ return the content of the file """
+        return ""
+    
+    def set_content(self):
+        """ set the contente of the file """
+        pass
+
+    def need_reload(self):
+        """ return True if the file has been modified since last time """
+        return False
+
+    def is_readonly(self):
+        return True
