73d1ba7d2774db620d03abc2b90967708a1b1274
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2013-04-24 20:15:37 +0200

    update help command (add global help)

--- devparrot/commands/help.py
+++ devparrot/commands/help.py
@@ -10,14 +10,35 @@ class inner:
         for line in helpText.split('\n'):
             yield "{}\n".format(line)
 
+    @staticmethod
+    def help_devparrot():
+        from devparrot.core import session
+        from devparrot.core.command.section import Section
+        commands = [key for key, command in session.commands.items() if not isinstance(command, Section)]
+        helps = []
+        yield "Devparrot help\n"
+        yield "==============\n"
+        yield "\n"
+        
+        yield "commands :\n"
+        yield "----------\n"
+        yield "\n"
+        for command in commands:
+            yield " - {}\n".format(command)
+
 Command(command=constraints.Command()
 )(inner.help, create_section("capi"))
+
+Command()(inner.help_devparrot, create_section("capi"))
 
 @Alias(
 command = constraints.Command(default= lambda:None)
 )
 def help(command):
     """The help command"""
-    commandName = command.get_name() if command else "help"
-    line = "capi.help {0!r} | capi.buffer 'help {0}'".format(commandName)
-    return line
+    if command:
+        commandName = command.get_name()
+        line = "capi.help {0!r} | capi.buffer 'help {0}'".format(commandName)
+        return line
+    else:
+        return "capi.help_devparrot | capi.buffer 'help devparrot'"
