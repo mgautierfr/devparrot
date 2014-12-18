7aa6ed7add0292feb91c62a62c52c8b301217745
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2014-12-18 17:31:04 +0100

    Add some debug subcommand

--- devparrot/commands/debug.py
+++ devparrot/commands/debug.py
@@ -20,8 +20,16 @@
 
 
 from devparrot.core.command import MasterCommand, SubCommand
-from devparrot.core.constraints import File
+from devparrot.core.constraints import File, Integer
 from devparrot.core.session import get_currentDocument
+from pprint import pprint
+import gc
+
+def count():
+    d = dict()
+    for o in gc.get_objects():
+        d[type(o).__name__] = d.get(type(o).__name__, 0)+1
+    pprint(d)
 
 class debug(MasterCommand):
 
@@ -35,3 +43,37 @@ class debug(MasterCommand):
             import pprint
             printer = pprint.PrettyPrinter(stream=f)
             printer.pprint(content)
+
+    @SubCommand(
+       ofile= File(mode=File.SAVE, default=lambda: "dump_info.txt")
+    )
+    def after_info(ofile):
+        """set a config entry to value"""
+        from devparrot.core import ui
+        content = ui.window.tk.call("after", "info")
+        #content = ui.window.after_info()
+        with open(ofile, "w") as f:
+            import pprint
+            printer = pprint.PrettyPrinter(stream=f)
+            printer.pprint(content)
+
+    @SubCommand(
+        #ofile = File(mode=File.SAVE, default=lambda: "backref.png"),
+    )
+    def back_ref():
+        import objgraph
+        gc.collect()
+        #classtypes = ('TextView', 'Document', 'RangeInfo', 'SourceBuffer', 'FileDocSource', 'DocumentView')
+        classtypes = ('TopContainer', 'Workspace', 'SplittedContainer', 'NotebookContainer', 'DocumentView')
+        #i = 0
+        #for o in objgraph.by_type(classtype):
+        #    l = objgraph.find_backref_chain(o, objgraph.is_proper_module)
+        #    objgraph.show_chain(l, filename="backref%d.png"%i)
+        #    i += 1
+        #print "test |%s|"% classtype
+        #count()
+        list_ = []
+        for type_ in classtypes:
+            list_.extend(objgraph.by_type(type_))
+        #print list_
+        objgraph.show_backrefs(list_, max_depth=3, too_many=100, filename="backref.png")
